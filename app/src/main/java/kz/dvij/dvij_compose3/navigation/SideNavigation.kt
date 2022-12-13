e982e394ee12f549226e4e550b2e2367630a39cf
McAr0v <dvijkz@gmail.com>
2022-12-13 16:27:54 +0600

    Потихой доделываю страницы связанны с аккаунтами

--- app/src/main/java/kz/dvij/dvij_compose3/navigation/SideNavigation.kt
+++ app/src/main/java/kz/dvij/dvij_compose3/navigation/SideNavigation.kt
@@ -2,15 +2,12 @@ package kz.dvij.dvij_compose3.navigation
 
 import android.content.Context
 import android.widget.Toast
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
@@ -19,6 +16,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -202,16 +200,20 @@ fun AvatarBoxSideNavigation(
             if (user.photoUrl != null) {
                 AsyncImage(
                     model = user.photoUrl,
-                    contentDescription = "",
+                    contentDescription = stringResource(id = R.string.icon_user_image),
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
                         .size(60.dp)
+                        .border(BorderStroke(2.dp, PrimaryColor), CircleShape)
                         .clip(CircleShape))
             } else {
                 androidx.compose.foundation.Image(
-                    painter = painterResource(id = kz.dvij.dvij_compose3.R.drawable.zhanna_avatar), // по идее сюда надо будет передавать из гугла, или иметь возможность загружать
+                    painter = painterResource(id = kz.dvij.dvij_compose3.R.drawable.no_user_image), // по идее сюда надо будет передавать из гугла, или иметь возможность загружать
+                    contentScale = ContentScale.Crop,
                     contentDescription = stringResource(id = kz.dvij.dvij_compose3.R.string.cd_avatar), // описание для слабовидящих
                     modifier = Modifier
                         .size(60.dp) // размер аватарки
+                        .border(BorderStroke(2.dp, PrimaryColor), CircleShape)
                         .clip(CircleShape) // делаем ее круглой
                 )
             }
--- app/src/main/java/kz/dvij/dvij_compose3/screens/ProfileScreen.kt
+++ app/src/main/java/kz/dvij/dvij_compose3/screens/ProfileScreen.kt
@@ -1,18 +1,19 @@
 package kz.dvij.dvij_compose3.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -20,12 +21,12 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.google.firebase.auth.FirebaseUser
-import kotlinx.coroutines.launch
 import kz.dvij.dvij_compose3.MainActivity
 import kz.dvij.dvij_compose3.R
 import kz.dvij.dvij_compose3.accounthelper.AccountHelper
-import kz.dvij.dvij_compose3.accounthelper.REGISTRATION
+import kz.dvij.dvij_compose3.navigation.LOG_IN_ROOT
 import kz.dvij.dvij_compose3.navigation.MEETINGS_ROOT
+import kz.dvij.dvij_compose3.navigation.REG_ROOT
 import kz.dvij.dvij_compose3.ui.theme.*
 
 // функция превью экрана
@@ -41,7 +42,8 @@ fun ProfileScreen (
     val scaffoldState = rememberScaffoldState()
     val accountHelper = AccountHelper(activity)
 
-    if (user!=null) {
+    // Условие, если user не равен null и не ожидает подтверждения email
+    if (user!=null && user.isEmailVerified) {
 
         Column(
             modifier = Modifier
@@ -52,109 +54,243 @@ fun ProfileScreen (
             verticalArrangement = Arrangement.Center
         ) {
 
-            // АВАТАРКА ПОЛЬЗОВАТЕЛЯ
+            // -------------- АВАТАРКА ПОЛЬЗОВАТЕЛЯ ------------------
 
             if (user.photoUrl != null) {
+
+                // Условие, если есть фотография пользователя
+
                 AsyncImage(
-                    model = user.photoUrl,
-                    contentDescription = "",
+                    model = user.photoUrl, // фотография пользователя из Google аккаунта
+                    contentScale = ContentScale.Crop, // увеличение изображения - либо по ширине либо по высоте выступающее за края части будут обрезаны
+                    contentDescription = stringResource(id = R.string.icon_user_image), // описание для слабовидящих
                     modifier = Modifier
-                        .size(150.dp)
-                        .clip(CircleShape))
+                        .size(150.dp) // размер аватарки
+                        .border(
+                            BorderStroke(4.dp, PrimaryColor), CircleShape
+                        ) // рамка вокруг иконки
+                        .clip(CircleShape)) // делаем аватарку круглой
             } else {
+
+                // Условие, если аватарки нет
+
                 androidx.compose.foundation.Image(
-                    painter = painterResource(id = R.drawable.zhanna_avatar), // по идее сюда надо будет передавать из гугла, или иметь возможность загружать
+                    painter = painterResource(id = R.drawable.no_user_image), // по идее сюда надо будет передавать из гугла, или иметь возможность загружать
+                    contentScale = ContentScale.Crop, // увеличение изображения - либо по ширине либо по высоте выступающее за края части будут обрезаны
                     contentDescription = stringResource(id = R.string.cd_avatar), // описание для слабовидящих
                     modifier = Modifier
-                        .size(130.dp) // размер аватарки
+                        .size(150.dp) // размер аватарки
+                        .border(
+                            BorderStroke(4.dp, PrimaryColor),
+                            CircleShape
+                        ) // рамка вокруг иконки
                         .clip(CircleShape) // делаем ее круглой
                 )
             }
 
-            Spacer(modifier = Modifier.height(30.dp)) // разделитель между заголовком и полями для ввода
+            Spacer(modifier = Modifier.height(30.dp)) // разделитель
+
+            // -------------- ИМЯ ПОЛЬЗОВАТЕЛЯ -----------------------
+
 
             if (user.displayName != null) {
 
-                Text(  // заголовок зависит от switch
-                    text = user.displayName!!,
-                    style = Typography.titleLarge, // стиль заголовка
-                    color = Grey00, // цвет заголовка
-                    textAlign = TextAlign.Center
+                Text(
+                    text = user.displayName!!, // имя из базы данных firebase
+                    style = Typography.titleLarge, // стиль текста
+                    color = Grey00, // цвет
+                    textAlign = TextAlign.Center // выравнивание по центру
                 )
 
             }
 
-            Spacer(modifier = Modifier.height(10.dp)) // разделитель между заголовком и полями для ввода
+            Spacer(modifier = Modifier.height(10.dp)) // разделитель
 
-            if (user.email != null){
+            // --------------- EMAIL ПОЛЬЗОВАТЕЛЯ ----------------
+
+            if (user.email != null) {
+
                 Text(
-                    text = user.email!! ,
+                    text = user.email!!, // email из базы данных firebase
                     style = Typography.labelMedium, // стиль текста
-                    textAlign = TextAlign.Center,
-                    color = Grey40
+                    textAlign = TextAlign.Center, // выравнивание по центру
+                    color = Grey40 // цвет
                 )
+            }
 
-                Spacer(modifier = Modifier.height(50.dp)) // разделитель между заголовком и полями для ввода
+                Spacer(modifier = Modifier.height(50.dp)) // разделитель
+
+
+                // --------- КНОПКА ВЫХОДА ИЗ АККАУНТА ------------
 
                 Button(
                     onClick = {
-                        activity.mAuth.signOut()
-                        accountHelper.signOutGoogle()
-                        coroutineScope.launch {
-                            scaffoldState.drawerState.close()
-                        }
-                        navController.navigate(MEETINGS_ROOT)
-                        Toast.makeText(activity, "Вы успешно вышли из системы", Toast.LENGTH_SHORT).show()
+                        // функции на нажатие
+
+                        activity.mAuth.signOut() // выход из аккаунта, если вошел по Email
+
+                        accountHelper.signOutGoogle() // выход из аккауна, если вошел через Google
+
+                        navController.navigate(MEETINGS_ROOT) // после выхода отправляем на страницу мероприятий
+
+                        // показываем ТОСТ что все готово
+
+                        Toast.makeText(
+                            activity,
+                            activity.getString(R.string.sign_out_success),
+                            Toast.LENGTH_SHORT
+                        ).show()
 
                     },
                     modifier = Modifier
-                        .fillMaxWidth().padding(horizontal = 30.dp),
+                        .fillMaxWidth() // кнопка занимает всю ширину
+                        .height(50.dp)// высота - 50
+                        .padding(horizontal = 30.dp), // отступы от краев
+                    shape = RoundedCornerShape(50), // скругление углов
                     colors = ButtonDefaults.buttonColors(
-                        backgroundColor = Grey00,
+
+                        // цвета кнопки
+                        backgroundColor = PrimaryColor,
                         contentColor = Grey100
+
                     )) {
 
-                    Text(text = "Выйти из аккаунта")
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_logout), // иконка
+                        contentDescription = stringResource(id = R.string.cd_icon), // описание для слабовидящих
+                        tint = Grey100 // цвет иконки
+                    )
 
-                }
+                    Spacer(modifier = Modifier.width(10.dp)) // разделитель между текстом и иконкой
+
+                    Text(
+                        text = stringResource(id = R.string.sign_out_button), // текст кнопки
+                        style = Typography.labelMedium // стиль текста
+                    )
+
 
             }
 
+        }
+
+    } else if (user!=null && !user.isEmailVerified){
+
+        Column(
+            modifier = Modifier
+                .background(Grey95)
+                .fillMaxSize()
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = stringResource(id = R.string.email_verify_toast), // имя из базы данных firebase
+                style = Typography.bodyLarge, // стиль текста
+                color = Grey00, // цвет
+                textAlign = TextAlign.Center // выравнивание по центру
+            )
+
+            Spacer(modifier = Modifier.height(40.dp)) // разделитель между текстом и иконкой
+
+            Button(
+                onClick = {
+                    // функции на нажатие
+
+                    navController.navigate(LOG_IN_ROOT)
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth() // кнопка занимает всю ширину
+                    .height(50.dp)// высота - 50
+                    .padding(horizontal = 30.dp), // отступы от краев
+                shape = RoundedCornerShape(50), // скругление углов
+                colors = ButtonDefaults.buttonColors(
+
+                    // цвета кнопки
+                    backgroundColor = PrimaryColor,
+                    contentColor = Grey100
+
+                )) {
+
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_login), // иконка
+                    contentDescription = stringResource(id = R.string.cd_icon), // описание для слабовидящих
+                    tint = Grey100 // цвет иконки
+                )
+
+                Spacer(modifier = Modifier.width(10.dp)) // разделитель между текстом и иконкой
+
+                Text(
+                    text = stringResource(id = R.string.to_login), // текст кнопки
+                    style = Typography.labelMedium // стиль текста
+                )
 
 
+            }
+
+        }
+
+    } else if (user == null) {
+
+        Column(
+            modifier = Modifier
+                .background(Grey95)
+                .fillMaxSize()
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = stringResource(id = R.string.need_registration_toast), // имя из базы данных firebase
+                style = Typography.bodyLarge, // стиль текста
+                color = Grey00, // цвет
+                textAlign = TextAlign.Center // выравнивание по центру
+            )
+
+            Spacer(modifier = Modifier.height(40.dp)) // разделитель между текстом и иконкой
+
+            Button(
+                onClick = {
+                    // функции на нажатие
+
+                    navController.navigate(REG_ROOT)
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth() // кнопка занимает всю ширину
+                    .height(50.dp)// высота - 50
+                    .padding(horizontal = 30.dp), // отступы от краев
+                shape = RoundedCornerShape(50), // скругление углов
+                colors = ButtonDefaults.buttonColors(
+
+                    // цвета кнопки
+                    backgroundColor = PrimaryColor,
+                    contentColor = Grey100
+
+                )) {
+
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_login), // иконка
+                    contentDescription = stringResource(id = R.string.cd_icon), // описание для слабовидящих
+                    tint = Grey100 // цвет иконки
+                )
+
+                Spacer(modifier = Modifier.width(10.dp)) // разделитель между текстом и иконкой
+
+                Text(
+                    text = stringResource(id = R.string.to_registration), // текст кнопки
+                    style = Typography.labelMedium // стиль текста
+                )
 
 
+            }
 
         }
 
     }
-}
 
-
-// экран профиля
-
-@Composable
-fun ProfileScreenContent (){
-
-    Column (
-        modifier = Modifier
-            .background(Grey10)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-
-        Text(text = "ProfileScreen", style = Typography.titleLarge)
-        Text(text = "ProfileScreen", style = Typography.titleMedium)
-        Text(text = "ProfileScreen", style = Typography.titleSmall)
-        Text(text = "ProfileScreen", style = Typography.bodyLarge)
-        Text(text = "ProfileScreen", style = Typography.bodyMedium)
-        Text(text = "ProfileScreen", style = Typography.bodySmall)
-        Text(text = "ProfileScreen", style = Typography.labelLarge)
-        Text(text = "ProfileScreen", style = Typography.labelMedium)
-        Text(text = "ProfileScreen", style = Typography.labelSmall)
-
-    }
 }
 
 
