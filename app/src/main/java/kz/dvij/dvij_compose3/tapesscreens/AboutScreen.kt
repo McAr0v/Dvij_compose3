9ff292fd14158dad49ebc5055be103f39b422b05
mcar0v <1990zhanna19901991>
2023-03-24 21:53:49 +0600

    Небольшие корректировки

--- app/src/main/java/kz/dvij/dvij_compose3/tapesscreens/AboutScreen.kt
+++ app/src/main/java/kz/dvij/dvij_compose3/tapesscreens/AboutScreen.kt
@@ -4,23 +4,34 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import kz.dvij.dvij_compose3.R
 import kz.dvij.dvij_compose3.ui.theme.Grey_Background
 import kz.dvij.dvij_compose3.ui.theme.Primary10
+import kz.dvij.dvij_compose3.ui.theme.Typography
+import kz.dvij.dvij_compose3.ui.theme.WhiteDvij
 
 @Composable
 fun AboutScreen(){
     Column (
         modifier = Modifier
             .background(Grey_Background)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .fillMaxSize()
+            .padding(horizontal = 20.dp, vertical = 30.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
     ) {
-        Text(text = "About")
+        Text(
+            text = stringResource(id = R.string.side_about),
+            color = WhiteDvij,
+            style = Typography.titleLarge
+        )
 
     }
 }
